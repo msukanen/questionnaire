69ad2b221213dfab2d20cbfddfb6e3cf7deba1cc
Markku Sukanen <markku.sukanen@gmail.com>
2021-09-22 09:18:21 +0300

    mods

---
+++ src/components/AuthorData.js
@@ -0,0 +1,5 @@
+export const Author = {
+    "name": "Markku Sukanen",
+    "email": "markku.sukanen@gmail.com",
+    "github": "http://github.com/msukanen/"
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types'
+import { Author } from './AuthorData'
 
 const Header = ({title}) => {
     return(
         <header className="App-header">
             <h1>{title}</h1>
-            <p>2021 by <em>Markku Sukanen</em></p>
+            <p>2021 by <em>{Author.name}</em></p>
         </header>
     )
 }
